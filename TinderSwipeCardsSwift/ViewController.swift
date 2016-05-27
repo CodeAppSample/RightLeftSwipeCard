7ca616ee0198f112456318847e46730cad58d55a
Storm <storm@storms-mac.local>
2016-05-27 08:45:14 -0700

    Initial Commit

---
+++ TinderSwipeCardsSwift/ViewController.swift
@@ -0,0 +1,22 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        let draggableBackground: DraggableViewBackground = DraggableViewBackground(frame: self.view.frame)
+        self.view.addSubview(draggableBackground)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
